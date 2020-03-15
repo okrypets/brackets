231581bb0f268c27b998941103a377cdadf9a109
Olga Studzenichnik <okrypets@gmail.com>
2020-03-15 22:51:17 +0300

    init: Brackets is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = 
+function check(str, bracketsConfig) {
+  
+  let regConfig = [...bracketsConfig].reduce((acc, it) => {
+    let strC = it[0].match(/\d/) ? "("+it[0]+it[1]+")" : "(\\"+it[0]+"\\"+it[1]+")";    
+
+    acc.push(strC)
+    return acc
+    }, []).join("|")
+  let newRegExpConfig = new RegExp(regConfig, 'g')
+  
+  let newStr = str;
+  let matchConfig = str.match(newRegExpConfig);;
+
+  do {
+    newStr = newStr.replace(newRegExpConfig, "")
+    matchConfig = newStr.match(newRegExpConfig);
+  } while (
+    matchConfig && matchConfig.length > 0
+  );
+
+  let result = newStr.length === 0
+  
+  return result;
 }
